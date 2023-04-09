6eacb5c6e69e24873b000656851fc1f45e2e0ced
Severyn <severyn.shevchukk@gmail.com>
2023-04-09 18:37:06 +0300

    Add project files.

---
+++ Patterns/Main.cs
@@ -0,0 +1,36 @@
+﻿using Patterns;
+using static System.Console;
+
+namespace Patterns
+{
+    internal class Patterns
+    {
+        static void Main(string[] args)
+        {
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 300);
+            drink.Consume();
+
+            WriteLine("\r\n");
+
+            ICar car = new CarProxy(new Driver(12));
+            car.Drive();
+
+            WriteLine("\r\n");
+
+            var ba = new BankAccount(100);
+            var m1 = ba.Deposit(50); // 150
+            var m2 = ba.Deposit(25); // 175
+            WriteLine(ba);
+
+            // restore to m1
+            ba.Restore(m1);
+            WriteLine(ba);
+
+            ba.Restore(m2);
+            WriteLine(ba);
+
+
+        }
+    }
+}
---
+++ Patterns/Momento.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace Patterns
+{
+    public class Memento
+    {
+        public int Balance { get; }
+
+        public Memento(int balance)
+        {
+            Balance = balance;
+        }
+    }
+
+    public class BankAccount
+    {
+        private int balance;
+
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+        }
+
+        public Memento Deposit(int amount)
+        {
+            balance += amount;
+            return new Memento(balance);
+        }
+
+        public void Restore(Memento m)
+        {
+            balance = m.Balance;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+}
